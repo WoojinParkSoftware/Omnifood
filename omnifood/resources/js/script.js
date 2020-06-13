e0d5c2c46449edcb186ede4ac085b6279a02f21d
woojeanss <pwj0223@gmail.com>
2020-06-14 06:26:00 +0900

    aye

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,13 +1,7 @@
-/* 
-var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-}) 
-*/
-
 $(document).ready(function(){
-
+    
+    /* for the sticky navigation */
+    
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -16,5 +10,69 @@ $(document).ready(function(){
         }
         offset: '60px;'
     });
+    
+    /* scroll on buttons */
+    
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body'). animate({scrollTop: $('js--section-plans').offset().top}, 1000);
+    });
+                                   
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body'). animate({scrollTop: $('js--section-features').offset().top}, 1000);
+    });
+         $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+    /* animations on scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+        
+    },  {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction){
+        
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+        
+    },  {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction){
+        
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+        
+    },  {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction){
+        
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+        
+    },  {
+        offset: '50%'
+    });
+    
 });
